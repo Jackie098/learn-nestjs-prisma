e6430c2d5721b19b9a722d24e3b28701955e2ede
Carlos Augusto <carlos.aug.developer@gmail.com>
2024-01-03 22:30:34 -0300

    created role guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Put,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user-dto';
@@ -16,32 +17,42 @@ import { UpdatePatchUserDTO } from './dto/update-patch-user-dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptor';
 import { ParamId } from 'src/decorators/param-id.decorator';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enum/role.enum';
+import { RoleGuard } from 'src/guards/role.guard';
+import { AuthGuard } from 'src/guards/auth.guard';
 
+@UseGuards(AuthGuard, RoleGuard)
 @UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Roles(Role.Admin)
   @Get()
   async list() {
     return this.userService.list();
   }
 
+  @Roles(Role.Admin)
   @Get(':id')
   async findOne(@ParamId() id: number) {
     return this.userService.findOne(id);
   }
 
+  @Roles(Role.Admin)
   @Post()
   async create(@Body() data: CreateUserDTO) {
     return this.userService.create(data);
   }
 
+  @Roles(Role.Admin)
   @Put(':id')
   async update(@ParamId() id: number, @Body() data: UpdateUserDTO) {
     return this.userService.update(id, data);
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   async updatePartial(
     @Param('id', ParseIntPipe) id: number,
@@ -50,6 +61,7 @@ export class UserController {
     return this.userService.updatePartial(id, data);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
     return this.userService.delete(id);
